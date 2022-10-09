1d93a7fa4cf4979244b4ad7e3ef3d8ba2f9174e2
Michael-Beskid <73857255+Michael-Beskid@users.noreply.github.com>
2022-10-09 01:36:04 -0400

    Update Test_Sketch.ino
    Added constants for physical design parameters. Implemented inverse kinematics from arc parameters to joint parameters.

--- Test_Sketch/Test_Sketch.ino
+++ Test_Sketch/Test_Sketch.ino
@@ -20,6 +20,7 @@ const int DXL_DIR_PIN = -1; // Dynamixel direction pin, set to -1 because not ne
 const float DXL_PROTOCOL_VERSION = 2.0; // Using Dynamixel protocol version 2.0
 const float FORCE_LIMIT = 15.0; // Force limit during homing operation [percent]
 const float HOMING_TIME = 2.0; // Time to pull on all tendons while homing [seconds]
+const float MAX_CURVATURE = 10.0; // Curvature limit to prevent damage from overextension [1/meters]
 
 // Variable Definitions
 float initialRot1 = 0.0; // Initial rotation of motor 1 after homing
@@ -27,11 +28,11 @@ float initialRot2 = 0.0; // Initial rotation of motor 2 after homing
 float initialRot3 = 0.0; // Initial rotation of motor 3 after homing
 
 // Design Parameters
-const float R_PRIME = 0.0; // Radial distance from spine to tendons [meters]
-const float NOTCH_HEIGHT = 0.0; // Height of cutout sections [meters]
-const float DISC_HEIGHT = 0.0; // Height of discs [meters]
-const int NUM_NOTCHES = 0; // Number of notched sections
-const float SPOOL_DIAMETER = 0.0; // Diameter of cable spools [meters]
+const float R_PRIME = 0.020; // Radial distance from spine to tendons [meters]
+const float NOTCH_HEIGHT = 0.030; // Height of cutout sections [meters]
+const float DISC_HEIGHT = 0.005; // Height of discs [meters]
+const int NUM_NOTCHES = 6; // Number of notched sections
+const float SPOOL_DIAMETER = 0.052; // Diameter of cable spools [meters]
 const float GAMMA = (2.0*PI)/3.0; // Angular distance between tendons [radians]
 
 // Dynamixel Configuration
@@ -87,7 +88,7 @@ void torqueDisable() {
 float angleToDeltaL(float angle) {
 
   // Calcualte tendon disaplcement from angle
-  deltaL = (angle/360)*PI*SPOOL_DIAMETER;
+  float deltaL = (angle/360)*PI*SPOOL_DIAMETER;
 
   // Return tendon displacement
   return deltaL;
@@ -106,7 +107,7 @@ float angleToDeltaL(float angle) {
 float deltaLtoAngle(float deltaL) {
 
   // Calcualte tendon disaplcement from angle
-  angle = (deltaL*360)/(PI*SPOOL_DIAMETER);
+  float angle = (deltaL*360)/(PI*SPOOL_DIAMETER);
   
   // Return tendon displacement
   return angle;
@@ -183,6 +184,25 @@ void currentMode() {
 
   // Enable motor torque
   torqueEnable();
+
+}
+
+/* Function: recordInitRotations
+ *  
+ *  Sets the initial rotation variables to the current angular positions of the motors.
+ *  
+ *  Params:
+ *    None
+ *    
+ *  Output:
+ *    None
+*/
+void recordInitRotations() {
+
+  // Set initial rotation angles for each of the three motors
+  initialRot1 = dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE);
+  initialRot2 = dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE);
+  initialRot3 = dxl.getPresentPosition(DXL3_ID, UNIT_DEGREE);
 
 }
 
@@ -330,24 +350,78 @@ void moveToPositionInSecs(float goalPos1, float goalPos2, float goalPos3, float 
 */
 void moveToPoseSecs(float rot1, float rot2, float rot3, float seconds) {
 
-  // TO DO: CURRENLTY I AM DOING THIS IN A "DUMB" WAY WITHOUT CHECKING POSITION. WORKS OKAY FOR NOW TO WITHIN A FEW DEGREES.
-  // I WILL UNCOMMMENT THESE LINES BELOW WHEN I ACTUALLY IMPLEMENT A CONTROL ALGORITHM TO HIT THE TARGET POSITIONS ACCURATELY.
+  // NOTE: THIS FUNCTION CHECKS THAT ALL MOTORS ACTUALLY REACH THEIR SETPOINTS.
+  //  CURRENTLY DOESN'T WORK ON THE ROBOT DUE TO INABILITY TO BEND ENOUGH.
 
-//  // Get current motor positions
-//  float startPos1 = dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE);
-//  float startPos2 = dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE);
-//  float startPos3 = dxl.getPresentPosition(DXL3_ID, UNIT_DEGREE);
-//
-//  // Compute target motor positions
-//  float targetPos1 = startPos1 + rot1;
-//  float targetPos1 = startPos2 + rot2;
-//  float targetPos1 = startPos3 + rot3;
+  // Get current motor positions
+  float startPos1 = dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE);
+  float startPos2 = dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE);
+  float startPos3 = dxl.getPresentPosition(DXL3_ID, UNIT_DEGREE);
+
+  // Compute target motor positions
+  float targetPos1 = startPos1 + rot1;
+  float targetPos2 = startPos2 + rot2;
+  float targetPos3 = startPos3 + rot3;
+
+  bool reachedTarget1 = false;
+  bool reachedTarget2 = false;
+  bool reachedTarget3 = false;
 
   // Calculate motor speeds as a function of distance to target
   //  Converts from dps to RPM
   float motorSpeed1 = rot1/(6*seconds);
   float motorSpeed2 = rot2/(6*seconds);
   float motorSpeed3 = rot3/(6*seconds);
+
+  DEBUG_SERIAL.print("motor speed 1: "); DEBUG_SERIAL.println(motorSpeed1);
+
+  // Command motor velocities to these values for given time
+  setMotorVelocities(motorSpeed1, motorSpeed2, motorSpeed3);
+
+  while (!reachedTarget1 || !reachedTarget2 || !reachedTarget3) {
+    if (abs(dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE) - targetPos1) < 1.0) {
+      reachedTarget1 = true;
+      dxl.setGoalVelocity(DXL1_ID, 0.0, UNIT_RPM);
+    }
+    if (abs(dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE) - targetPos2) < 1.0) {
+      reachedTarget2 = true;
+      dxl.setGoalVelocity(DXL2_ID, 0.0, UNIT_RPM);
+    }
+    if (abs(dxl.getPresentPosition(DXL3_ID, UNIT_DEGREE) - targetPos3) < 1.0) {
+      reachedTarget3 = true;
+      dxl.setGoalVelocity(DXL3_ID, 0.0, UNIT_RPM);
+    }
+  }
+
+  // Set motor velocities to zero
+  stopMotors();
+
+}
+
+/* Function: moveToPoseInSecs
+ *  
+ *  Sets motor target positions for use in VELOCITY mode. Then stops motors to hold position.
+ *  
+ *  Params:
+ *    float rot1 -> desired rotation for motor 1 in DEGREES
+ *    float rot2 -> desired rotation for motor 2 in DEGREES
+ *    float rot3 -> desired rotation for motor 3 in DEGREES
+ *    float seconds -> time to complete movement in SECONDS
+ *    
+ *  Output:
+ *    None
+*/
+void moveToPoseSecs2(float rot1, float rot2, float rot3, float seconds) {
+
+  // NOTE: THIS FUNCTION IS "DUMB" IN THAT IT COMMANDS MOTIONS BUT DOESN'T USE FEEDBACK
+
+  // Calculate motor speeds as a function of distance to target
+  //  Converts from dps to RPM
+  float motorSpeed1 = rot1/(6*seconds);
+  float motorSpeed2 = rot2/(6*seconds);
+  float motorSpeed3 = rot3/(6*seconds);
+
+  DEBUG_SERIAL.print("motor speed 1: "); DEBUG_SERIAL.println(motorSpeed1);
 
   // Command motor velocities to these values for given time
   setMotorVelocities(motorSpeed1, motorSpeed2, motorSpeed3);
@@ -363,41 +437,88 @@ void moveToPoseSecs(float rot1, float rot2, float rot3, float seconds) {
  *  Commands robot to move to pose specified by the desired base angle and curvature.
  *  
  *  Params:
- *    float baseAngle -> desired base rotation angle in DEGREES
- *    float curvature -> desired robot curvature [1/m]
+ *    float phi -> desired base rotation angle in DEGREES
+ *    float K -> desired robot curvature [1/m]
  *    float seconds -> time to complete movement in SECONDS
  *    
  *  Output:
  *    None
 */
-void setArcParameters(float baseAngle, float curvature, float seconds) {
+void setArcParameters(float phi, float K, float seconds) {
 
-  // TO DO: Compute desired tendon disaplcements from input arc parameters
-      // CAN DO THIS FROM THE MODELING WE DERIVED
+  DEBUG_SERIAL.print("K = "); DEBUG_SERIAL.println(K);
 
-  float deltaL1 = 0.0;
-  float deltaL2 = 0.0;
-  float deltaL3 = 0.0;
+  // Limit the input curvature to the pre-set bounded value
+  if (K > MAX_CURVATURE) {
+    K = MAX_CURVATURE;
+  }
 
-  // Calculate rotations for each motor corresponding to tendon displacements
+  // Create variables for the angle setpoints for the three motors
+  //    Angles remain at zero if specified curvature is zero
+  float angle1 = 0.0;
+  float angle2 = 0.0;
+  float angle3 = 0.0;
+
+  // Compute motor angular positions from input arc parameters
+  if (K != 0) {
+
+    // Calculate radius of curvature and angle for an individual notch
+    float R = 1/K;
+    float theta = NOTCH_HEIGHT*K;
+
+    // Calculate the distance of each tendon from the robot centerline in the place of curvature
+    float r1 = R_PRIME*cos(phi);
+    float r2 = R_PRIME*cos(phi + GAMMA);
+    float r3 = R_PRIME*cos(phi - GAMMA);
+
+    // Calculate the height of the tendons for the specified configurations (for one notch)
+    float t1 = 2*(R-r1)*sin(theta/2);
+    float t2 = 2*(R-r2)*sin(theta/2);
+    float t3 = 2*(R-r3)*sin(theta/2);
+
+    // Calculate desired tendon displacement for each notch (for one notch)
+    float deltaL1 = -(NOTCH_HEIGHT - t1);
+    float deltaL2 = -(NOTCH_HEIGHT - t2);
+    float deltaL3 = -(NOTCH_HEIGHT - t3);
+
+    // Get the rotation angle that equates to the desired total tendon disaplcement
+    angle1 = deltaLtoAngle(deltaL1*NUM_NOTCHES);
+    angle2 = deltaLtoAngle(deltaL2*NUM_NOTCHES);
+    angle3 = deltaLtoAngle(deltaL3*NUM_NOTCHES);
+
+    // Temporary: Print a bunch of stuff for debugging purposes
+    DEBUG_SERIAL.print("phi = "); DEBUG_SERIAL.println(phi);
+    DEBUG_SERIAL.print("R = "); DEBUG_SERIAL.println(R);
+    DEBUG_SERIAL.print("theta = "); DEBUG_SERIAL.println(theta);
+    DEBUG_SERIAL.print("r1 = "); DEBUG_SERIAL.println(r1);
+    DEBUG_SERIAL.print("t1 = "); DEBUG_SERIAL.println(t1);
+    DEBUG_SERIAL.print("deltaL1 = "); DEBUG_SERIAL.println(deltaL1);
+    
+  }
+
+  // Calculate rotations for each motor to achieve desired angular positions
   
-  float angle1 = deltaLtoAngle(deltaL1*NUM_NOTCHES); // Get the total rotation angle that equates to the desired tendon disaplcement
   float adjustedAngle1 = angle1 + initialRot1; // Subtract the initial angle of the motor to get the absolute angular position we want
   float currentAngle1 = dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE); // Read the current angular position of the motor
   float rot1 = adjustedAngle1 - currentAngle1; // Compute the difference between the current and target angles to get rotation amount needed
 
-  float angle2 = deltaLtoAngle(deltaL2*NUM_NOTCHES);
   float adjustedAngle2 = angle2 + initialRot2;
   float currentAngle2 = dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE);
   float rot2 = adjustedAngle2 - currentAngle2;
 
-  float angle3 = deltaLtoAngle(deltaL3*NUM_NOTCHES);
   float adjustedAngle3 = angle3 + initialRot3;
   float currentAngle3 = dxl.getPresentPosition(DXL3_ID, UNIT_DEGREE);
   float rot3 = adjustedAngle3 - currentAngle3;
 
+  //Temporary: print a bunch of stuff for debugging purposes
+  DEBUG_SERIAL.print("angle1 = "); DEBUG_SERIAL.println(angle1);
+  DEBUG_SERIAL.print("initialRot1 = "); DEBUG_SERIAL.println(initialRot1);
+  DEBUG_SERIAL.print("adjustedAngle1 = "); DEBUG_SERIAL.println(adjustedAngle1);
+  DEBUG_SERIAL.print("currentAngle1 = "); DEBUG_SERIAL.println(currentAngle1);
+  DEBUG_SERIAL.print("rot1 = "); DEBUG_SERIAL.println(rot1);
+
   // Execute a moveToPoseSecs command to hit the specified pose over the input time interval
-  moveToPoseSecs(rot1, rot2, rot3, seconds);
+  moveToPoseSecs2(rot1, rot2, rot3, seconds);
 
 }
 
@@ -423,6 +544,9 @@ void homeConfig() {
 
   // Stop after specified homing time
   delay(HOMING_TIME*1000);
+
+  // Store initial angular position of motors
+  recordInitRotations();
 
   // Hold motors in their current positions
   stopMotors();
@@ -452,56 +576,61 @@ void setup() {
 
   // Move robot to home position
   delay(1000);
-//  homeConfig();
-//  DEBUG_SERIAL.println("Done moving to home position.");
-
+  homeConfig();
+  DEBUG_SERIAL.println("Done moving to home position.");
   delay(1000);
-  stopMotors();
-  
+
+  // THIS COMMENTED SECTION IS A TEST FOR moveToPoseSecs FUNCTION
+
+//  float motor1start = dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE);
+//  float motor2start = dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE);
+//  
+//  DEBUG_SERIAL.print("Motor 1 Start Angle: ");
+//  DEBUG_SERIAL.println(motor1start);
+//  DEBUG_SERIAL.print("Motor 2 Start Angle: ");
+//  DEBUG_SERIAL.println(motor2start);
+//  
+//  moveToPoseSecs(90.0, 180.0, 0.0, 2.0);
+//  
+//  float motor1end = dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE);
+//  float motor2end = dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE);
+//
+//  DEBUG_SERIAL.print("Motor 1 End Angle: ");
+//  DEBUG_SERIAL.println(motor1end);
+//  DEBUG_SERIAL.print("Motor 2 End Angle: ");
+//  DEBUG_SERIAL.println(motor2end);
+//
+//  DEBUG_SERIAL.print("Motor 1 Rotation: ");
+//  DEBUG_SERIAL.println(motor1end - motor1start);
+//  DEBUG_SERIAL.print("Motor 2 Rotation: ");
+//  DEBUG_SERIAL.println(motor2end - motor2start);
 }
 
 void loop() {
 
-  // Temporary: This loop just spins the motors back and forth and prints angular positions to validate the "moveToPoseSecs" function
-
-  DEBUG_SERIAL.print("Motor 1 Start Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.print("Motor 2 Start Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.println("");
-  
-  moveToPoseSecs(100.0, 200.0, 0.0, 2.0);
-  
-  DEBUG_SERIAL.print("Motor 1 End Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.print("Motor 2 End Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.println("");
-  
+  setArcParameters(0,10,1);
+  DEBUG_SERIAL.println("Position 1");
   delay(2000);
-
-  DEBUG_SERIAL.print("Motor 1 Start Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.print("Motor 2 Start Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.println("");
-  
-  moveToPoseSecs(-200.0, -100.0, 0.0, 2.0);
-  
-  DEBUG_SERIAL.print("Motor 1 End Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL1_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.print("Motor 2 End Position: ");
-  DEBUG_SERIAL.print(dxl.getPresentPosition(DXL2_ID, UNIT_DEGREE));
-  DEBUG_SERIAL.println(" degrees");
-  DEBUG_SERIAL.println("");
-
+  setArcParameters(0,0,1);
+  DEBUG_SERIAL.println("Home");
+  delay(2000);
+  setArcParameters(PI/2,10,1);
+  DEBUG_SERIAL.println("Position 2");
+  delay(2000);
+  setArcParameters(0,0,1);
+  DEBUG_SERIAL.println("Home");
+  delay(2000);
+  setArcParameters(PI,10,1);
+  DEBUG_SERIAL.println("Position 3");
+  delay(2000);
+  setArcParameters(0,0,1);
+  DEBUG_SERIAL.println("Home");
+  delay(2000);
+  setArcParameters((3.0*PI)/2.0,10,1);
+  DEBUG_SERIAL.println("Position 3");
+  delay(2000);
+  setArcParameters(0,0,1);
+  DEBUG_SERIAL.println("Home");
   delay(2000);
  
 }
